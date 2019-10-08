e65508e3132002d8938c2886af5ce3465d0b2427
Willster419 <willster419@outlook.com>
2019-10-07 19:49:26 -0700

    Added sample description designer

---
+++ RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RelhaxDescriptionDesigner.Designs;
+
+namespace RelhaxDescriptionDesigner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WillstersFirstWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Designs.Willster419.FirstFlowDocument firstFlowDocument = new Designs.Willster419.FirstFlowDocument();
+            firstFlowDocument.Show();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Uri.ToString());
+        }
+    }
+}
