55701bd6c698689164348db5470353138144c42a
Arkhorse <theillusivemman@gmail.com>
2019-10-14 12:22:48 -0300

    PMOD start (For real this time)

---
+++ RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/Designs/TheIllusion/PmodWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RelhaxDescriptionDesigner.Designs.TheIllusion
+{
+    /// <summary>
+    /// Interaction logic for PmodWindow.xaml
+    /// </summary>
+    public partial class PmodWindow : Window
+    {
+        public PmodWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
