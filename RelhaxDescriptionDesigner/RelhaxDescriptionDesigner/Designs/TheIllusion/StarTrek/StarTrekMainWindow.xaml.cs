3897619dab6da5e1698847712603addf739d87ef
Arkhorse <theillusivemman@gmail.com>
2019-10-17 18:30:52 -0300

    Finished Star Trek Disc functionality

--- RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/Designs/TheIllusion/StarTrek/StarTrekMainWindow.xaml.cs
+++ RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/Designs/TheIllusion/StarTrek/StarTrekMainWindow.xaml.cs
@@ -26,5 +26,25 @@ namespace RelhaxDescriptionDesigner.Designs.TheIllusion.StarTrek
         private void CloseWindow_CanExec(object sender, CanExecuteRoutedEventArgs e) => e.CanExecute = true;
 
         private void CloseWindow_Exec(object sender, ExecutedRoutedEventArgs e) => SystemCommands.CloseWindow(this);
+        private void Click_ST_Crew(object sender, RoutedEventArgs e)
+        {
+            ST_Crew stCrew = new ST_Crew();
+            stCrew.Show();
+        }
+        private void Click_ST_Camo(object sender, RoutedEventArgs e)
+        {
+            ST_Camo stCamo = new ST_Camo();
+            stCamo.Show();
+        }
+        private void Click_ST_LoadingScreen(object sender, RoutedEventArgs e)
+        {
+            ST_LoadingScreens stLoad = new ST_LoadingScreens();
+            stLoad.Show();
+        }
+        private void Click_ST_GunSounds(object sender, RoutedEventArgs e)
+        {
+            ST_GunSounds stGun = new ST_GunSounds();
+            stGun.Show();
+        }
     }
 }
