772f9299f035c1906f184cf8ee80e015233182e5
Arkhorse <theillusivemman@gmail.com>
2019-10-19 06:31:05 -0300

    Added close button to loading screens disc, started camo disc

--- RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/Designs/TheIllusion/StarTrek/ST_Camo.xaml.cs
+++ RelhaxDescriptionDesigner/RelhaxDescriptionDesigner/Designs/TheIllusion/StarTrek/ST_Camo.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using Microsoft.Win32;
 
 namespace RelhaxDescriptionDesigner.Designs.TheIllusion.StarTrek
 {
